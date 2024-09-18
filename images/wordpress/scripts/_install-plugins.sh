0625dc2ca2bfb7fcaf33453ebc45753e45d891de
Edie Lemoine <edie@myparcel.nl>
2024-09-18 17:35:06 +0200

    fix(wordpress): fix ssl always being forced and plugins not installing

--- images/wordpress/scripts/_install-plugins.sh
+++ images/wordpress/scripts/_install-plugins.sh
@@ -1,6 +1,8 @@
 install-plugins() {
   h1 "Linking, installing and activating plugins..."
 
+  PLUGINS_FOUND=$(find "$TMP_PLUGINS_DIR" -type d -mindepth 1 -maxdepth 1)
+
   link-paths "$TMP_PLUGINS_DIR" "$WP_PLUGINS_DIR"
 
   if [ -n "$WP_SKIP_PLUGINS" ]; then
@@ -8,11 +10,28 @@ install-plugins() {
     return
   fi
 
-  for path in "$TMP_PLUGINS_DIR"/*/; do
-    plugin=$(basename "$path")
+  if [ -z "$PLUGINS_FOUND" ]; then
+    h2 "No local plugins to install."
+  else
+    h2 "Installing local plugins..."
+    for path in $PLUGINS_FOUND; do
+      plugin=$(basename "$path")
 
-    wp-activate plugin "$plugin"
-  done
+      wp-activate plugin "$plugin"
+    done
+  fi
+
+  PLUGINS_LIST=$(echo "$WP_PLUGINS" | tr ',' '\n')
+
+  if [ -z "$PLUGINS_LIST" ]; then
+    h2 "No remote plugins to install."
+  else
+    h2 "Downloading and installing remote plugins..."
+
+    for plugin in $PLUGINS_LIST; do
+      wp-activate plugin "$plugin"
+    done
+  fi
 
   h2 "Finished installing and activating plugins."
 }
