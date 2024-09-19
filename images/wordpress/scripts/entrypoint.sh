3125f1cfb29202d142101db80461fad31df7297d
Edie Lemoine <edie@myparcel.nl>
2024-09-19 11:51:56 +0200

    fix(wordpress): fix ssl always being forced and plugins not installing (#16)

--- images/wordpress/scripts/entrypoint.sh
+++ images/wordpress/scripts/entrypoint.sh
@@ -2,9 +2,6 @@
 
 declare -i term_width=70
 
-export TMP_PLUGINS_DIR="/tmp/plugins"
-export WP_PLUGINS_DIR="$ROOT_DIR/wp-content/plugins"
-
 for script in /tmp/scripts/_*; do source $script; done
 
 # Start php fpm in the background in advance.
